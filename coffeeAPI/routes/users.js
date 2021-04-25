fc045157ea4a32e501221e9e4d2a53d8d95d3f44
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2021-04-24 23:58:04 -0600

    user info

--- coffeeAPI/routes/users.js
+++ coffeeAPI/routes/users.js
@@ -7,41 +7,42 @@ const confJWT = require('../conf/jwt.conf')
 
 /* GET users listing. */
 router.post('/login', async function (req, res, next) {
-  var username = req.body.username  
-  var password = crypto.createHash('md5').update(req.body.password).digest('hex')
-  var role = req.body.role
+  var username = req.body.username
+  var password = crypto.createHash('md5').update(req.body.password).digest('hex')  
 
-  const exists = await userModel.findOne({ username: username, password: password, role: role })
-  
+  const exists = await userModel.findOne({ username: username, password: password })
+
   if (exists) {
     let token = jwt.sign({ check: true }, confJWT.key, { expiresIn: '1h' })
-    res.status(200).json({ _id: exists._id, username: username, role: role, token: token })
+    res.status(200).json({ _id: exists._id, username: username, role: exists.role, phone: exists.phone, address: exists.address, token: token })
   } else {
-    res.status(401).send('Unauthorized')
+    res.status(401).json({ status : 401, message: 'Unauthorized'})
   }
 });
 
 router.post('/create', async function (req, res, next) {
   try {
 
-    const exists = await userModel.findOne({ username: req.body.username })    
+    const exists = await userModel.findOne({ username: req.body.username })
 
     if (!exists) {
       const user = new userModel({
         username: req.body.username,
+        address: req.body.address,
+        phone: req.body.phone,
         password: crypto.createHash('md5').update(req.body.password).digest('hex'),
         role: req.body.role
       })
 
       await user.save()
-      res.status(200).send('Successfully created')
+      res.status(200).json({status: 200, message: 'Successfully created'})
     } else {
-      res.status(409).send('A user with that username already exists')  
+      res.status(202).json({ status: 202, message: 'A user with that username already exists' })
     }
-  
+
   } catch (error) {
     console.log(error);
-    res.status(500).send(error)
+    res.status(500).json({status: 500, message: error})    
   }
 })
 
